02d907ab75f2813ecd3b1769cefa956b0fdab35d
Andrew Virts <asvirts@outlook.com>
2025-01-12 08:50:21 -0600

    Update heading styling for responsive design by adjusting h1 text size for smaller screens, enhancing readability and user experience on the homepage.

--- andrew/app/page.tsx
+++ andrew/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <Badge>Automate sales. Eliminate busywork.</Badge>
-                <h1 className="sm:text-4xl md:text-9xl font-bold tracking-tighter py-4">
+                <h1 className="xs:text-4xl md:text-9xl font-bold tracking-tighter py-4">
                   High-Performing Websites and Web Applications
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400 py-2">

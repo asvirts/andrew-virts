4e86db8baf5c5e00bbd60e8af9c3cf0cebc8b443
Andrew Virts <asvirts@outlook.com>
2025-01-11 18:37:41 -0600

    Update footer copyright to reflect the current year and author name

--- andrew/app/page.tsx
+++ andrew/app/page.tsx
@@ -211,7 +211,7 @@ export default function Home() {
       </main>
       <footer className="max-w-[1400px] flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2024 Acme Web Design. All rights reserved.
+          © {new Date().getFullYear()} Andrew Virts. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
